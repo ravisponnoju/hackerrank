236c0e980a1be21bf5c4d54b71fdd9b4a8c471cd
ravisponnoju <ravisponnoju@gmail.com>
2017-06-11 21:03:08 -0500

    practise

---
+++ languages/linux-shell/bash/comparing_numbers.sh
@@ -0,0 +1,10 @@
+read num1
+read num2
+
+if [ $num1 -gt $num2 ]; then
+    echo "X is greater than Y"
+elif [ $num1 -lt $num2 ]; then
+    echo "X is less than Y"
+else
+    echo "X is equal to Y"
+fi  
