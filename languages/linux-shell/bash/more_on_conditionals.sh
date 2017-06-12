8afcb3b4c7176569d71385550772f20e76c9be39
ravisponnoju <ravisponnoju@gmail.com>
2017-06-11 21:03:54 -0500

    practise

---
+++ languages/linux-shell/bash/more_on_conditionals.sh
@@ -0,0 +1,13 @@
+read side1
+read side2
+read side3
+
+if [[ $side1 == $side2 ]] && [[ $side1 == $side3 ]] && [[ $side2 == $side3 ]]; then
+    echo "EQUILATERAL"
+
+elif [[ $side1 == $side2 ]] || [[ $side1 == $side3 ]] || [[ $side2 == $side3 ]]; then
+    echo "ISOSCELES"
+
+else
+    echo "SCALENE"
+fi
