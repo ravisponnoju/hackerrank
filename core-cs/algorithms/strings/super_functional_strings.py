467bbbe959fe45741a8769ad216f5009d59ecef3
ravisponnoju <ravisponnoju@gmail.com>
2017-08-01 11:48:56 -0500

    in progress

--- core-cs/algorithms/strings/super_functional_strings.py
+++ core-cs/algorithms/strings/super_functional_strings.py
@@ -1,30 +1,33 @@
 from collections import Counter
+import sys
 import timeit
 
+sys.setrecursionlimit(10000)
 start = timeit.default_timer()
 
-T = int(raw_input().strip())
 
+def calc_func_value(s_arr, sub_s_hash):
+
+    if len(s_arr) == 0:
+        return 0
+
+    s_str = ''.join(s_arr)
+    if s_str not in sub_s_hash:
+        sub_s_hash.add(s_str)
+        return len(s_arr) ** len(Counter(s_arr).keys()) + \
+                calc_func_value(s_arr[:-1], sub_s_hash) + \
+                calc_func_value(s_arr[1:], sub_s_hash)
+    else:
+        return 0
+
+T = int(raw_input().strip())
 
 for t in xrange(T):
     s = raw_input().strip()
     s_list = list(s)
 
-    F = 0
-    string_hash = set([])
-    # print s_list
+    sub_string_hash = set([])
+    print calc_func_value(s_list, sub_string_hash)
 
-    for i in xrange(len(s)):
-        for j in xrange(i, len(s)):
-            string = ''.join(s_list[i:j + 1])
-
-            if string not in string_hash:
-                string_hash.add(string)
-                string_counter = Counter(string)
-                # print string, string_counter
-
-                F += len(string) ** len(string_counter.keys())
-
-    print F
 
 print timeit.default_timer() - start
